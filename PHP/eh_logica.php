8f8f8b53c6f9e923c7499d54f59fd55ec98b90ac
Marc <rais2004pod@gmail.com>
2025-03-18 09:35:51 +0100

    first commit login

---
+++ PHP/eh_logica.php
@@ -0,0 +1,15 @@
+<!-- 
+OBJETIVO:
+
+· Maquetació responsiva del front-end amb tècniques SEO.
+    · Utilitzar marques estructurals correctament en tot el codi.
+    · Utilitzar atributs per informar del contingut de les imatges.
+    · Utilitzar atributs per informar del contingut dels links.
+    · Validar formularis amb codi HTML.
+· Codificar un disseny responsiu amb CSS.
+-->
+<?php
+
+
+
+?>
