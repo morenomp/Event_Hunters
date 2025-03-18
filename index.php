b9b23373ad5498fddae2dbf44dad45f983508f2c
Marc <rais2004pod@gmail.com>
2025-03-18 09:40:41 +0100

    second commit login

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Login de la gestora catalana Event Hunters">
+    <meta name="keywords" content="Event Hunters">
+    <meta name="author" content="Marc Moreno y Adrian Palma">
+    <meta name="copyright" content="propiedades del copyright Event Hunters">
+    <title>Login - Event Hunters</title>
+
+    <link rel="stylesheet" href="CSS/style.css">
+
+    <!-- <link rel="icon" type="image/png" href="IMG/favicon.png"> -->
+</head>
+
+<!-- 
+OBJETIVO:
+
+· Maquetació responsiva del front-end amb tècniques SEO.
+    · Utilitzar marques estructurals correctament en tot el codi.
+    · Utilitzar atributs per informar del contingut de les imatges.
+    · Utilitzar atributs per informar del contingut dels links.
+    · Validar formularis amb codi HTML.
+· Codificar un disseny responsiu amb CSS.
+-->
+<body>
+    
+    <!-- Contenedor inicial de "crear una cuenta" -->
+    <div>
+
+    <!--  -->
+        <div>
+            <h2>CREA</h2>
+            <h2>UNA</h2>
+            <h2>CUENTA</h2>
+        </div>
+
+        <form action="eh_logica.php" method="post">
+            
+        </form>
+    </div>
+
+</body>
+</html>
