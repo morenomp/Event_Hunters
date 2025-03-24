046ad3205131195d716d1bfee662fef317f336bf
Marc <rais2004pod@gmail.com>
2025-03-24 09:45:13 +0100

    estructura de carpetas

---
+++ CONTROLLER/eh_logica.php
@@ -0,0 +1,24 @@
+<!-- 
+OBJETIVOS:
+
+· Un único formulario de login
+· Comprobar los datos (si son correctos en la base de datos)
+· Usaremos MySQLI
+· Si no funciona/es incorrecto, nos quedaremos en la misma página y saldrá un mesanje de error
+· Si estas logueado puedes hacer logout, si no (en incognito por ejemplo) no dejará
+· Tenemnos que hacer que el usuario admin pueda ver cosas que un usuario normal no puede
+
+· Deben haber dos formularios diferentes para un admin y para un usuario normal
+· Si registramos un usuario que ya existe debe dar error
+· Solo el admin puede subir una imagen de perfil
+· Al darle a logout limpiaremos todo
+
+· Toda la información se debe guardar en MySQL
+· Se debe usar un único php llamado userController, que hará registro, login y logout
+· Añadir validaciones
+-->
+<?php
+
+
+
+?>
